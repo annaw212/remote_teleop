8d436f55dc9eea96ba2f415646c4c827dacedf5e
Anna Wong <annawong@gmail.com>
2022-07-21 14:59:01 -0700

    Fix file that somehow got deleted

--- src/action_remote_teleop_server.py
+++ src/action_remote_teleop_server.py
@@ -2,14 +2,29 @@
 
 import rospy
 import actionlib
-from action_remote_teleop.msg import TurnInPlaceAction, TurnInPlaceGoal, TurnInPlaceFeedback, TurnInPlaceResult
+
+from action_remote_teleop.msg import TurnInPlaceAction, TurnInPlaceGoal, TurnInPlaceResult
 
 class TurnInPlaceClass():
-	
-	# create messages that are used to publish feedback/results
-	_feedback = TurnInPlaceFeedback()
-	_result = TurnInPlaceResult()
-	
-	def __init__(self):
-		# creates the action server
-		self._actionServer = 
+
+    # create messages that are used to publish result
+    _result = TurnInPlaceResult()
+
+    def __init__(self)
+        # create the action server and start it
+        self._turn_in_place_server = actionlib.SimpleActionServer("turn_in_place_as", TurnInPlaceAction, self.callback, False)
+        self._turn_in_place_server.start()
+
+    def callback(self, msg):
+        #TODO: put something here
+        print(msg)
+
+if __name__ == '__main__':
+    # initialize the node
+    rospy.init_node('remote_teleop_node', anonymous=True)
+
+    # initialize the class
+    turn_in_place = TurnInPlaceClass()
+
+    # keep the program from exiting before the node is stopped
+    rospy.spin()
