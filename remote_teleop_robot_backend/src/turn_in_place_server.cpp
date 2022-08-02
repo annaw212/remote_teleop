1147a3ce5b87696cfc482ddfd0aaafc6daca7485
Anna Wong <anna.wong@zebra.com>
2022-08-02 11:57:14 -0700

    Comment out some debugging printouts -- will be cleaned up later

--- remote_teleop_robot_backend/src/turn_in_place_server.cpp
+++ remote_teleop_robot_backend/src/turn_in_place_server.cpp
@@ -4,8 +4,6 @@
  */
 
 #include <ros/ros.h>
-#include "turn_in_place_server.h"
-
 #include <iostream>
 #include <tf/tf.h>
 #include <ros/rate.h>
@@ -17,7 +15,10 @@
 #include <remote_teleop_robot_backend/TurnInPlaceGoal.h>
 #include <remote_teleop_robot_backend/TurnInPlaceResult.h>
 
+#include "turn_in_place_server.h"
+
 /*-----------------------------------------------------------------------------------*/
+
 // Define variables here
 #define THRESHOLD 0.08
 #define MIN_VEL 0.08
@@ -80,8 +81,6 @@ void TurnInPlace::initializeActions() {
 /*-----------------------------------------------------------------------------------*/
 
 void TurnInPlace::turn_in_place_callback(const remote_teleop_robot_backend::TurnInPlaceGoalConstPtr& goal) {
-
-//  ROS_INFO("Turn in place callback");
   
   // TODO: gray out rviz plugin buttons when turn is being executed
   
@@ -91,8 +90,6 @@ void TurnInPlace::turn_in_place_callback(const remote_teleop_robot_backend::Turn
     
   // Convert from degrees to radians
   angle_ = angle_ * M_PI / 180;
-  
-//  ROS_INFO("Angle = %f, Turn left = %d", angle_, turn_left_);
   
   // TODO: update vel vars here, OR move this somewhere else
   lin_vel_ = 0.0;
@@ -110,7 +107,6 @@ void TurnInPlace::turn_in_place_callback(const remote_teleop_robot_backend::Turn
 /*-----------------------------------------------------------------------------------*/
 
 void TurnInPlace::odom_callback(const nav_msgs::Odometry& msg) {
-//  ROS_INFO("Getting odometry");
   
   // Grab the odometry quaternion values out of the message
   tf::Quaternion q(
@@ -125,15 +121,11 @@ void TurnInPlace::odom_callback(const nav_msgs::Odometry& msg) {
   // Extract the euler angles from the matrix
   m.getRPY(roll_, pitch_, yaw_);
   
-//  ROS_INFO("Yaw = %f, Pitch = %f, Roll = %f", yaw_, pitch_, roll_);
-  
 }
 
 /*-----------------------------------------------------------------------------------*/
 
 void TurnInPlace::turn_in_place() {
-  
-//  ROS_INFO("Turn in place function");
   
   // Create message to be sent
   geometry_msgs::Twist command;
@@ -148,27 +140,21 @@ void TurnInPlace::turn_in_place() {
   
   float goal_yaw = 0.0;
   
-//  ROS_INFO("CURRENT ANGLE = %f", yaw_);
-//  ROS_INFO("ANGULAR VELOCITY = %f", ang_vel_);
-    
+
   if(turn_left_ == false) {
     // TURNING RIGHT
     goal_yaw = yaw_ - angle_;
-//    ROS_INFO("Raw goal angle = %f", goal_yaw);
     // Make sure the goal angle is within a valid range
     if(goal_yaw < -M_PI) {
       goal_yaw += 2*M_PI;
     }
-//    ROS_INFO("Goal angle = %f", goal_yaw);
   } else {
     // TURNING LEFT
     goal_yaw = yaw_ + angle_;
-//    ROS_INFO("Raw goal angle = %f", goal_yaw);
     // Make sure the goal angle is within a valid range
     if(goal_yaw > M_PI) {
       goal_yaw -= 2*M_PI;
     }
-//    ROS_INFO("Goal angle = %f", goal_yaw);
   }
   
   // Turn the robot until it reaches the desired angle
@@ -179,41 +165,29 @@ void TurnInPlace::turn_in_place() {
     
     // Ensure the robot will be turning in the correct direction
     if(turn_left_ == true && command.angular.z < 0.0) {
-      command.angular.z *= -1;
-      if (command.angular.z < MIN_VEL) {
-        command.angular.z = MIN_VEL;
-      } 
-      
+      command.angular.z *= -1;      
     } else if(turn_left_ == false && command.angular.z > 0.0) {
       command.angular.z *= -1;
-      if (command.angular.z > -MIN_VEL) {
-        command.angular.z = -MIN_VEL;
-      } 
     }
     
+    // Ensure the robot will never be turning at a speed greater than the desired angular velocity
     if (turn_left_ == true && command.angular.z > ang_vel_) {
       command.angular.z = ang_vel_;
-    } else if (turn_left_ == true && abs(command.angular.z) > ang_vel_) {
+    } else if (turn_left_ == false && abs(command.angular.z) > ang_vel_) {
       command.angular.z = -ang_vel_;
+    } else if (turn_left_ == true && command.angular.z < MIN_VEL) {
+      command.angular.z = MIN_VEL;
+    } else if (turn_left_ == false && abs(command.angular.z) < MIN_VEL) {
+      command.angular.z = -MIN_VEL;
     }
-    // TODO: get rid of this
-//    ROS_INFO("%f", command.angular.z);
-//    ROS_INFO("%f\t%f\t%f", goal_yaw, yaw_, command.angular.z);
     
     // Publish the message to the drivers
     turn_in_place_publisher_.publish(command);
   }
   
-//  ROS_INFO("Goal angle = %f, Current angle = %f", goal_yaw, yaw_);
-  
-  // Stop the robot from moving farther
+  // Stop the robot once it has reached its goal
   command.angular.z = 0.0;
-  turn_in_place_publisher_.publish(command);
-//  ROS_INFO("%f", command.angular.z);
-  
-//  ROS_INFO("Exited the while loop.");
-  
-  
+  turn_in_place_publisher_.publish(command);  
 }
 
 /*-----------------------------------------------------------------------------------*/
