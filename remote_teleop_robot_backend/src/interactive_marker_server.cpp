c9ee229be0f8913251b99484526cc216fdbcf98b
Anna Wong <anna.wong@zebra.com>
2022-08-04 10:21:07 -0700

    Update interactive marker server

--- remote_teleop_robot_backend/src/interactive_marker_server.cpp
+++ remote_teleop_robot_backend/src/interactive_marker_server.cpp
@@ -33,6 +33,8 @@
 
 #include <interactive_markers/interactive_marker_server.h>
 
+/*----------------------------------------------------------------------------------------------*/
+
 void processFeedback(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
 {
@@ -41,12 +43,15 @@ void processFeedback(
       << ", " << feedback->pose.position.z );
 }
 
+/*----------------------------------------------------------------------------------------------*/
+
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "simple_marker");
+  
+  ros::init(argc, argv, "test_interactive_marker");
 
   // create an interactive marker server on the topic namespace simple_marker
-  interactive_markers::InteractiveMarkerServer server("simple_marker");
+  interactive_markers::InteractiveMarkerServer server("test_interactive_marker");
 
   // create an interactive marker for our server
   visualization_msgs::InteractiveMarker int_marker;
