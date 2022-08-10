d0ca9f1b1ecae28a243dc5c4a41af9b0c7f2cb1a
Anna Wong <anna.wong@zebra.com>
2022-08-10 15:31:33 -0700

    Fix math

--- remote_teleop_robot_backend/src/turn_in_place_server.cpp
+++ remote_teleop_robot_backend/src/turn_in_place_server.cpp
@@ -511,7 +511,9 @@ void TurnInPlace::navigate(float angle, bool turn_left, float x_dist, float y_di
     // Drive straight
     while (abs(goal_x - x_) > THRESHOLD && abs(goal_y - y_)) {
 //      ROS_INFO_STREAM(abs(goal_dist - x_));
+      ROS_INFO_STREAM(x_dist << ", " << y_dist);
       ROS_INFO_STREAM(x_ << ", " << y_);
+      ROS_INFO_STREAM(goal_x << ", " << goal_y);
       goal_x = x_;
       goal_y = y_;
       // Set the linear velocity
