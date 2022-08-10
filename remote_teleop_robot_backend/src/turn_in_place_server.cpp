9a1749a7091ae986ddea2a9666cd01f534729795
Anna Wong <anna.wong@zebra.com>
2022-08-10 15:14:04 -0700

    Add in lines to make robot move

--- remote_teleop_robot_backend/src/turn_in_place_server.cpp
+++ remote_teleop_robot_backend/src/turn_in_place_server.cpp
@@ -511,7 +511,7 @@ void TurnInPlace::navigate(float angle, bool turn_left, float dist) {
     // Drive straight
     while (abs(goal_dist - x_) > THRESHOLD) {
       ROS_INFO_STREAM(abs(goal_dist - x_));
-      ROS_INFO_STREAM(x_ << ", " << y_ << ", " << z_)
+      ROS_INFO_STREAM(x_ << ", " << y_ << ", " << z_);
       goal_dist = x_;
       // Set the linear velocity
       command.linear.x = lin_vel_ * (goal_dist - x_);
