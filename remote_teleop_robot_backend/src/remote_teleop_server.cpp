7fd6cfe6773da647d7a0f31e6a3db22dad42172f
Anna Wong <anna.wong@zebra.com>
2022-08-15 15:21:12 -0700

    Attempt to fix costmap errors yeesh

--- remote_teleop_robot_backend/src/remote_teleop_server.cpp
+++ remote_teleop_robot_backend/src/remote_teleop_server.cpp
@@ -607,9 +607,6 @@ int main(int argc, char** argv) {
 
   ROS_INFO("Main: instantiating an object of type TurnInPlace");
   
-  tf2_ros::Buffer tl(ros::Duration(0));
-  costmap_2d::Costmap2DROS costmap("costmap_rt", tl);
-  
   TurnInPlace remote_teleop_class;
   
   ROS_INFO("Main: going into spin");
