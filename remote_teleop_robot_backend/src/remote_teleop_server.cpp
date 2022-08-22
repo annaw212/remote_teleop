b24e30d4dd9d0d6dd5694a500cc912dc09aa647e
Anna Wong <anna.wong@zebra.com>
2022-08-22 11:19:40 -0700

    Trying to print out true value of the occupancy grid

--- remote_teleop_robot_backend/src/remote_teleop_server.cpp
+++ remote_teleop_robot_backend/src/remote_teleop_server.cpp
@@ -776,6 +776,9 @@ void RemoteTeleop::obstacleCheck(float x1, float y1, float x2, float y2,
       ROS_INFO("SAFE TO NAVIGATE");
     }
     
+    occupancy_grid_debug_.data[idx] = 100;
+    occupancy_grid_debug_publisher_.publish(occupancy_grid_debug_);
+//  ros::spinOnce();
     // checking either to decrement or increment the value
     // if we have to plot from (0,100) to (100,0)
     x1 < x2 ? x1++ : x1--;
